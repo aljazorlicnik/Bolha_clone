b2b2ddd2d4d4562a31a5f35223b21d9584cdc1b7
Aljaž Orličnik <aljaz.orlicnik@gmail.com>
2023-09-26 09:21:31 +0200

    utf dejanski fix

--- baza.php
+++ baza.php
@@ -8,10 +8,10 @@ $password="7#5pg9}6)nl?";
 $db="orlieu_bolha";
 
 $link = mysqli_connect($host, $user, $password, $db);
-mysql_set_charset("UTF8", $conn);
 
 // Check connection
 if (!$link) {
   die("Povezava ni uspela: " . mysqli_connect_error());
 }
+mysqli_set_charset($link, "utf8")
 ?>

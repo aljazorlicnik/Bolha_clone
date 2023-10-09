6e7646d46233fc7bfc237494bf1723054daec5d4
Aljaž Orličnik <aljaz.orlicnik@gmail.com>
2023-10-10 01:05:17 +0200

    Update reg_in.php

--- reg_in.php
+++ reg_in.php
@@ -2,34 +2,43 @@
 require_once('baza.php');
 require_once('cookie.php');
 
-$i = $_POST['ime'];
-$pri = $_POST['priimek'];
-$e = $_POST['e-mail'];
-$p = $_POST['geslo'];
+// Check if all required fields are set
+if(isset($_POST['ime'], $_POST['priimek'], $_POST['e-mail'], $_POST['geslo'])) {
+    $i = $_POST['ime'];
+    $pri = $_POST['priimek'];
+    $e = $_POST['e-mail'];
+    $p = $_POST['geslo'];
 
-// Use password_hash
-$kp = password_hash($p, PASSWORD_DEFAULT);
+    // Use password_hash
+    $kp = password_hash($p, PASSWORD_DEFAULT);
 
-$google_id = isset($_SESSION['google_id']) ? $_SESSION['google_id'] : null;
+    $google_id = isset($_SESSION['google_id']) ? $_SESSION['google_id'] : null;
 
-$prev = "SELECT * FROM uporabniki WHERE `email` = '$e';";
-$result = $pdo->query($prev);
+    // Use prepared statements to avoid SQL injection
+    $stmt = $pdo->prepare("SELECT * FROM uporabniki WHERE email = ?");
+    $stmt->execute([$e]);
 
-if ($result->rowCount() === 1) {
-    header("Refresh:0;url=registracija.php");
-    echo '<script>alert("Uporabnik s tem e-poštnim naslovom že obstaja.")</script>';
-} else {
-    if ($google_id !== null) {
-        $query = "INSERT INTO uporabniki (ime, priimek, email, geslo, google_id) VALUES ('$i', '$pri', '$e', '$kp', '$google_id');";
-    } else {
-        $query = "INSERT INTO uporabniki (ime, priimek, email, geslo) VALUES ('$i', '$pri', '$e', '$kp');";
-    }
-
-    if ($pdo->query($query)) {
-        header("Refresh:0;url=prijava.php");
-    } else {
+    if ($stmt->rowCount() === 1) {
         header("Refresh:0;url=registracija.php");
-        echo '<script>alert("Registracija ni uspela.")</script>';
+        echo '<script>alert("Uporabnik s tem e-poštnim naslovom že obstaja.")</script>';
+    } else {
+        if ($google_id !== null) {
+            $query = "INSERT INTO uporabniki (ime, priimek, email, geslo, google_id) VALUES (?, ?, ?, ?, ?)";
+            $params = [$i, $pri, $e, $kp, $google_id];
+        } else {
+            $query = "INSERT INTO uporabniki (ime, priimek, email, geslo) VALUES (?, ?, ?, ?)";
+            $params = [$i, $pri, $e, $kp];
+        }
+
+        $stmt = $pdo->prepare($query);
+        if ($stmt->execute($params)) {
+            header("Refresh:0;url=prijava.php");
+        } else {
+            header("Refresh:0;url=registracija.php");
+            echo '<script>alert("Registracija ni uspela.")</script>';
+        }
     }
+} else {
+    echo "Missing required fields.";
 }
 ?>

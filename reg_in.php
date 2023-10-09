6b6f88d950a63ebc389bc7173e9e3c92c50966aa
Aljaž Orličnik <aljaz.orlicnik@gmail.com>
2023-10-10 00:59:13 +0200

    Update reg_in.php

--- reg_in.php
+++ reg_in.php
@@ -10,26 +10,26 @@ $p = $_POST['geslo'];
 // Use password_hash
 $kp = password_hash($p, PASSWORD_DEFAULT);
 
+$google_id = isset($_SESSION['google_id']) ? $_SESSION['google_id'] : null;
 
+$prev = "SELECT * FROM uporabniki WHERE `email` = '$e';";
+$result = $pdo->query($prev);
 
-    $prev = "SELECT * FROM uporabniki WHERE `email` = '$e';";
-    $result = $pdo->query($prev);
-
-    if ($result->num_rows === 1) {
-        header("Refresh:0;url=registracija.php");
-        echo '<script>alert("Uporabnik s tem e-poštnim naslovom že obstaja.")</script>';
-    }
-    else {
-        if(isset($_SESSION['google_id'])
-           {
-               $query = "INSERT INTO uporabniki (ime, priimek, email, geslo, google_id) VALUES ('$i', '$pri', '$e', '$kp', '$_SESSION['google_id']');";
-           }
+if ($result->rowCount() === 1) {
+    header("Refresh:0;url=registracija.php");
+    echo '<script>alert("Uporabnik s tem e-poštnim naslovom že obstaja.")</script>';
+} else {
+    if ($google_id !== null) {
+        $query = "INSERT INTO uporabniki (ime, priimek, email, geslo, google_id) VALUES ('$i', '$pri', '$e', '$kp', '$google_id');";
+    } else {
         $query = "INSERT INTO uporabniki (ime, priimek, email, geslo) VALUES ('$i', '$pri', '$e', '$kp');";
-        if ($pdo->query($query) === TRUE) {
-            header("Refresh:0;url=prijava.php");
-        } else {
-            header("Refresh:0;url=registracija.php");
-            echo '<script>alert("Registracija ni uspela.")</script>';
-        }
     }
+
+    if ($pdo->query($query)) {
+        header("Refresh:0;url=prijava.php");
+    } else {
+        header("Refresh:0;url=registracija.php");
+        echo '<script>alert("Registracija ni uspela.")</script>';
+    }
+}
 ?>

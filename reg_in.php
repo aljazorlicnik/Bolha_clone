f3654241663bc5b520be0f09bec058cd8900a08c
Aljaž Orličnik <aljaz.orlicnik@gmail.com>
2023-10-10 00:58:02 +0200

    Update reg_in.php

--- reg_in.php
+++ reg_in.php
@@ -10,17 +10,10 @@ $p = $_POST['geslo'];
 // Use password_hash
 $kp = password_hash($p, PASSWORD_DEFAULT);
 
-try {
-    // Establish a database connection using credentials from baza.php
-    $conn = new mysqli($host, $user, $password, $db);
 
-    // Check the connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
 
     $prev = "SELECT * FROM uporabniki WHERE `email` = '$e';";
-    $result = $conn->query($prev);
+    $result = $pdo->query($prev);
 
     if ($result->num_rows === 1) {
         header("Refresh:0;url=registracija.php");
@@ -32,17 +25,11 @@ try {
                $query = "INSERT INTO uporabniki (ime, priimek, email, geslo, google_id) VALUES ('$i', '$pri', '$e', '$kp', '$_SESSION['google_id']');";
            }
         $query = "INSERT INTO uporabniki (ime, priimek, email, geslo) VALUES ('$i', '$pri', '$e', '$kp');";
-        if ($conn->query($query) === TRUE) {
+        if ($pdo->query($query) === TRUE) {
             header("Refresh:0;url=prijava.php");
         } else {
             header("Refresh:0;url=registracija.php");
             echo '<script>alert("Registracija ni uspela.")</script>';
         }
     }
-
-    // Close the database connection
-    $conn->close();
-} catch (Exception $e) {
-    die("Error: " . $e->getMessage());
-}
 ?>
